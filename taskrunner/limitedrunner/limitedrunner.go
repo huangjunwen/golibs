3f8dd469d33134092ac28381654f96aa89694039
huang jw <kassarar@gmail.com>
2020-06-28 07:27:41 +0800

    Add taskrunner (#1)
    Reimplement taskrunner

---
+++ taskrunner/limitedrunner/limitedrunner.go
@@ -0,0 +1,204 @@
+package taskrunner
+
+import (
+	"fmt"
+	"sync"
+	"time"
+
+	. "github.com/huangjunwen/golibs/taskrunner"
+)
+
+const (
+	// Default minimum go routines to handle tasks.
+	DefaultMinWorkers = 2
+
+	// Default maximum go routines to handle tasks.
+	DefaultMaxWorkers = 4096
+
+	// Default queue size.
+	DefaultQueueSize = 16 * 1024
+
+	// Default idle time for non-persistent worker before quit.
+	DefaultIdleTime = 30 * time.Second
+)
+
+var (
+	nop            = func() {}
+	_   TaskRunner = (*LimitedRunner)(nil)
+)
+
+// LimitedRunner implements taskrunner interface. It starts with some persistent
+// worker go routines (MinWorkers) which will not exit until Close.
+// New worker go routines (up to MaxWorkers - MinWorkers)
+// maybe created when workload increases, and will exit after some
+// idle time (IdleTime).
+//
+// Tasks are submitted to a buffered channel (size is QueueSize) and distrubuted to all workers.
+type LimitedRunner struct {
+	minWorkers int // at least 1
+	maxWorkers int // at least minWorkers
+	queueSize  int // at least 1
+	idleTime   time.Duration
+
+	workerCh chan struct{} // to limit the number of workers
+	taskCh   chan func()   // buffered task channel
+	wg       sync.WaitGroup
+
+	mu     sync.RWMutex
+	closed bool
+}
+
+// New creates a new LimitedRunner.
+func New(opts ...Option) (*LimitedRunner, error) {
+	r := &LimitedRunner{
+		minWorkers: DefaultMinWorkers,
+		maxWorkers: DefaultMaxWorkers,
+		queueSize:  DefaultQueueSize,
+		idleTime:   DefaultIdleTime,
+	}
+
+	for _, opt := range opts {
+		if err := opt(r); err != nil {
+			return nil, err
+		}
+	}
+
+	if r.maxWorkers < r.minWorkers {
+		return nil, fmt.Errorf("New: MaxWorkers(%d) < MinWorkers(%d)", r.maxWorkers, r.minWorkers)
+	}
+
+	r.workerCh = make(chan struct{}, r.maxWorkers)
+	r.taskCh = make(chan func(), r.queueSize)
+
+	for i := 0; i < r.minWorkers; i++ {
+		r.workerCh <- struct{}{}
+		r.wg.Add(1)
+		go r.workerLoop(true, nop)
+	}
+
+	if r.maxWorkers > r.minWorkers {
+		r.wg.Add(1)
+		go r.managerLoop()
+	}
+
+	return r, nil
+}
+
+// managerLoop is used to fork non-persistent worker go routines.
+func (r *LimitedRunner) managerLoop() {
+	defer r.wg.Done()
+
+	for {
+		r.workerCh <- struct{}{} // We need worker quota.
+		task := <-r.taskCh
+		if task == nil {
+			<-r.workerCh
+			return
+		}
+		r.wg.Add(1)
+		go r.workerLoop(false, task)
+	}
+}
+
+// workerLoop handles task until taskCh closed, or idle long enough
+// for non-persistent workers.
+func (r *LimitedRunner) workerLoop(persistent bool, task func()) {
+
+	defer func() {
+		// Release worker quota.
+		<-r.workerCh
+		r.wg.Done()
+	}()
+
+	// NOTE: idleCh is nil for persistent worker so that it will never trigger a idle timeout (and exit).
+	// stopTimer should be invoked only when idleCh has NOT yet drained.
+	// resetTimer should be invoked only on stopped or expired timers with drained channels.
+	idleCh := (<-chan time.Time)(nil)
+	stopTimer := nop
+	resetTimer := nop
+
+	if !persistent {
+		idleTimer := time.NewTimer(r.idleTime)
+
+		idleCh = idleTimer.C
+		stopTimer = func() {
+			if !idleTimer.Stop() {
+				<-idleTimer.C
+			}
+		}
+		resetTimer = func() {
+			idleTimer.Reset(r.idleTime)
+		}
+
+		stopTimer()
+	}
+
+	for {
+		func() {
+			defer func() {
+				// Recover silently.
+				recover()
+			}()
+			task()
+		}()
+
+		resetTimer()
+		select {
+		case task = <-r.taskCh:
+			stopTimer()
+			if task == nil {
+				return
+			}
+
+		case <-idleCh:
+			// This branch is impossible for persistent worker.
+			return
+		}
+	}
+
+}
+
+// Submit implements taskrunner interface. Returns ErrTooBusy if task queue
+// (the buffered channel) is full at this moment.
+func (r *LimitedRunner) Submit(task func()) error {
+	if task == nil {
+		panic(fmt.Errorf("LimitedRunner.Submit(nil)"))
+	}
+
+	r.mu.RLock()
+	defer r.mu.RUnlock()
+
+	if r.closed {
+		return ErrClosed
+	}
+
+	select {
+	case r.taskCh <- task:
+		return nil
+
+	default:
+		return ErrTooBusy
+	}
+
+}
+
+// Close implements taskrunner interface. Returns when all submitted task finished.
+func (r *LimitedRunner) Close() {
+
+	r.mu.Lock()
+	if !r.closed {
+		r.closed = true
+		close(r.taskCh)
+	}
+	r.mu.Unlock()
+
+	// Wait workers and manager.
+	r.wg.Wait()
+
+	if l := len(r.taskCh); l != 0 {
+		panic(fmt.Errorf("len(taskCh) = %d in Close()", l))
+	}
+	if l := len(r.workerCh); l != 0 {
+		panic(fmt.Errorf("len(workerCh) = %d in Close()", l))
+	}
+}
---
+++ taskrunner/limitedrunner/limitedrunner_test.go
@@ -0,0 +1,250 @@
+package taskrunner
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+
+	. "github.com/huangjunwen/golibs/taskrunner"
+)
+
+func TestNew(t *testing.T) {
+	assert := assert.New(t)
+
+	{
+		_, err := New(MinWorkers(0))
+		assert.Error(err)
+	}
+	{
+		_, err := New(MaxWorkers(0))
+		assert.Error(err)
+	}
+	{
+		_, err := New(QueueSize(0))
+		assert.Error(err)
+	}
+	{
+		_, err := New(MinWorkers(2), MaxWorkers(1))
+		assert.Error(err)
+	}
+	{
+		_, err := New(IdleTime(-time.Second))
+		assert.Error(err)
+	}
+}
+
+func TestSubmit(t *testing.T) {
+	assert := assert.New(t)
+
+	r, err := New(
+		MinWorkers(1),
+		MaxWorkers(2),
+		QueueSize(1),
+	)
+	assert.NoError(err)
+	defer r.Close()
+
+	// The task returns only when close(stopCh)
+	syncCh := make(chan int)
+	stopCh := make(chan struct{})
+	task := func(i int) func() {
+		return func() {
+			syncCh <- i
+			<-stopCh
+		}
+	}
+
+	assert.NoError(r.Submit(task(1))) // 1
+	assert.Equal(1, <-syncCh)         // sync 1
+
+	assert.NoError(r.Submit(task(2))) // 2
+	assert.Equal(2, <-syncCh)         // sync 2
+
+	assert.NoError(r.Submit(task(3))) // 3 enqueued but no worker can handle this
+	//<-syncCh                        // sync 3 will be dead
+
+	assert.Equal(ErrTooBusy, r.Submit(task(4))) // 4 fail since queue full
+
+	close(stopCh)             // 1 and 2 done
+	assert.Equal(3, <-syncCh) // sync 3
+
+	r.Close()
+
+	assert.Equal(ErrClosed, r.Submit(task(5))) // 5 fail since closed
+}
+
+func TestClose(t *testing.T) {
+	assert := assert.New(t)
+
+	maxWorkers := 15
+	taskCount := 30
+
+	r, err := New(
+		MaxWorkers(maxWorkers),
+	)
+	assert.NoError(err)
+	defer r.Close()
+
+	mu := &sync.Mutex{}
+	count := 0
+	task := func() {
+		mu.Lock()
+		count++
+		c := count
+		mu.Unlock()
+		fmt.Printf("task %d\n", c)
+		time.Sleep(time.Second)
+	}
+
+	start := time.Now()
+	for i := 0; i < taskCount; i++ {
+		assert.NoError(r.Submit(task))
+	}
+	fmt.Printf("tasks submitted\n")
+	r.Close() // Close should wait all submitted tasks finished
+	dur := time.Since(start)
+
+	assert.Equal(taskCount, count)
+	fmt.Printf("%d tasks in %s\n", count, dur.String())
+
+}
+
+func TestIdleTime(t *testing.T) {
+	assert := assert.New(t)
+
+	r, err := New(
+		MaxWorkers(4294967296),         // Unlimited workers.
+		IdleTime(500*time.Millisecond), // Short idle time.
+		//IdleTime(10*time.Millisecond), // Short idle time.
+	)
+	assert.NoError(err)
+	defer r.Close()
+
+	wg := &sync.WaitGroup{}
+	stopCh := make(chan struct{})
+
+	// Print number of go routines each interval.
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+
+		ticker := time.NewTicker(200 * time.Millisecond)
+		defer ticker.Stop()
+
+		for {
+			select {
+			case t := <-ticker.C:
+				fmt.Printf("[%s] go routines: %d\n", t.String(), runtime.NumGoroutine())
+
+			case <-stopCh:
+				return
+			}
+		}
+
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		defer close(stopCh)
+
+		taskCountPerSecond := 20480
+		interval := 100 * time.Millisecond
+
+		tickerInSecond := int(time.Second / interval)
+		taskCountPerInterval := taskCountPerSecond / tickerInSecond
+
+		ticker := time.NewTicker(interval)
+		defer ticker.Stop()
+
+		i := 0
+		for taskCountPerInterval > 0 {
+			<-ticker.C
+
+			// Submit taskCountPerInterval tasks to the runner.
+			for j := 0; j < taskCountPerInterval; j++ {
+				assert.NoError(r.Submit(nop))
+			}
+
+			// Each second reduce taskCountPerInterval to half.
+			if i%tickerInSecond == 0 {
+				taskCountPerInterval /= 2
+			}
+			i++
+
+		}
+
+	}()
+
+	wg.Wait()
+
+	fmt.Printf("[%s] go routines before close: %d\n", time.Now().String(), runtime.NumGoroutine())
+	r.Close()
+	fmt.Printf("[%s] go routines after close: %d\n", time.Now().String(), runtime.NumGoroutine())
+
+}
+
+func TestPanic(t *testing.T) {
+	assert := assert.New(t)
+
+	r, err := New(
+		MinWorkers(1),
+		MaxWorkers(1),
+		QueueSize(1),
+	)
+	assert.NoError(err)
+	defer r.Close()
+
+	// Task can't be nil
+	assert.Panics(func() {
+		r.Submit(nil)
+	})
+
+	// The first panic task should no affect the later one.
+	syncCh := make(chan struct{})
+	assert.NoError(r.Submit(func() {
+		syncCh <- struct{}{}
+		panic(fmt.Errorf("xxxx"))
+	}))
+
+	<-syncCh
+	laterCalled := false
+	assert.NoError(r.Submit(func() {
+		fmt.Println("task after panic")
+		laterCalled = true
+	}))
+
+	r.Close()
+	assert.True(laterCalled)
+}
+
+func TestSubmitInTask(t *testing.T) {
+	assert := assert.New(t)
+
+	r, err := New(
+		MinWorkers(1),
+		MaxWorkers(1),
+		QueueSize(1),
+	)
+	assert.NoError(err)
+	defer r.Close()
+
+	laterCalled := false
+	syncCh := make(chan struct{})
+
+	assert.NoError(r.Submit(func() {
+		assert.NoError(r.Submit(func() {
+			fmt.Println("submit inside task")
+			laterCalled = true
+		}))
+		syncCh <- struct{}{}
+	}))
+
+	<-syncCh
+	r.Close()
+	assert.True(laterCalled)
+}
