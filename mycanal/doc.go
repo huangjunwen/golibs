e2786c82eb96ad636cf8e4744f58db01c541a6b6
jayven <kassarar@gmail.com>
2020-06-28 10:57:55 +0800

    Start to add mycanal

---
+++ mycanal/doc.go
@@ -0,0 +1,32 @@
+// Package mycanal is a CDC (Change Data Capture) library for MySQL8+.
+// No need to maintain table schemas manually any more.
+//
+// It provides helper functions for full dump (package fulldump) and incremental change capture (package incrdump).
+// Prerequisites:
+//   - MySQL-8.0.2 and above
+//   - GTID mode enabled:
+//     - `--gtid-mode=ON`
+//     - `--enforce-gtid-consistency=ON`
+//   - binlog enabled with the following:
+//     - `--log-bin=xxxx`: enable bin log
+//     - `--server-id=xxx`: the server id
+//     - `--binlog-format=ROW`: binlog output row changes instead of statments
+//     - `--binlog-row-image=FULL`: before and after image of row changes
+//     - `--binlog-row-metadata=FULL`: extra optional meta for tables such as signedness for numeric columns/column names ...
+//
+// ref:
+//   - https://mysqlhighavailability.com/more-metadata-is-written-into-binary-log/
+//   - https://mysqlhighavailability.com/taking-advantage-of-new-transaction-length-metadata/
+//   - https://github.com/siddontang/go-mysql/pull/468
+//   - https://github.com/siddontang/go-mysql/pull/482
+//
+// Compatiable between fulldump/incrdump (see tests/compatiable_test.go):
+//
+//   - DECIMAL/NUMERIC fields are returned as string, but may have different trailing zeros.
+//   - BINARY fields are returned as string, but may have different trailing '\x00'.
+//   - JSON fields are returned as string, but elements inside may have different position.
+//
+//   - BIT fields seems have some bugs in incrdump.
+//   - TIME with fraction seems have some bugs in incrdump too.
+//   - GEOMETRY fields are not tested
+package mycanal
---
+++ mycanal/fulldump/query.go
@@ -0,0 +1,65 @@
+package fulldump
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/pkg/errors"
+
+	"github.com/huangjunwen/golibs/sqlh"
+)
+
+// RowIter is used for result set iteration. It returns nil if no more row.
+// Caller should invoke RowIter(false) to close the iterator and release resource.
+type RowIter func(next bool) (map[string]interface{}, error)
+
+// Query and returns RowIter
+func Query(ctx context.Context, q sqlh.Queryer, query string, args ...interface{}) (iter RowIter, err error) {
+
+	rows, err := q.QueryContext(ctx, query, args...)
+	if err != nil {
+		return nil, errors.WithMessage(err, "fulldump.Query error")
+	}
+	defer func() {
+		if err != nil {
+			rows.Close()
+		}
+	}()
+
+	names, err := rows.Columns()
+	if err != nil {
+		return nil, errors.WithMessage(err, "fulldump.Query get Columns error")
+	}
+
+	makeScanValues := makeScanValues(rows)
+	values := []interface{}(nil)
+
+	return func(next bool) (map[string]interface{}, error) {
+		if !next {
+			return nil, errors.WithMessage(rows.Close(), "fulldump.Query close rows error")
+		}
+
+		if !rows.Next() {
+			return nil, errors.WithMessage(rows.Err(), "fulldump.Query rows error")
+		}
+
+		values = makeScanValues(values)
+		if err := rows.Scan(values...); err != nil {
+			return nil, errors.WithMessage(err, "fulldump.Query scan error")
+		}
+		postProcessScanedValues(values)
+
+		m := make(map[string]interface{})
+		for i, name := range names {
+			m[name] = values[i]
+		}
+
+		return m, nil
+	}, nil
+}
+
+// FullTableQuery full dump a table.
+func FullTableQuery(ctx context.Context, q sqlh.Queryer, dbName, table string) (RowIter, error) {
+	query := fmt.Sprintf("SELECT * FROM %s.%s", dbName, table)
+	return Query(ctx, q, query)
+}
